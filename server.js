45364af2433957a06ec67267132a8b727f7a2c29
Mats-ste <mms080607@gmail.com>
2026-04-24 12:13:38 +0200

    V4

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const session  = require('express-session');
 const path     = require('path');
 
 const app  = express();
-const PORT = 3000;
+const PORT = 287;
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
